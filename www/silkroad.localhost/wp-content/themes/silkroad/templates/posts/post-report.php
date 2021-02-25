9f1e493a26087fb142fbe3eab25e62b11a439e40
Rectangle <studio@rectangle.design>
2021-02-25 12:15:14 +0000

    Chapter nums

--- www/silkroad.localhost/wp-content/themes/silkroad/templates/posts/post-report.php
+++ www/silkroad.localhost/wp-content/themes/silkroad/templates/posts/post-report.php
@@ -1,5 +1,15 @@
+<?php
+
+$is_chapter = $args['is_chapter'];
+$chapter_num = $args['chapter_num'];
+
+?>
 <article class="report">
 	<div class="content-box">
+
+		<?php if ($is_chapter): ?>
+			<h3><?php echo silkroad_translate('chapter-n', LANG, [$chapter_num]); ?>
+		<?php endif; ?>
 		<h3><?php echo silkroad_translate_field(get_field('title_ml_text')); ?></h3>
 		<?php echo silkroad_translate_field(get_field('content_ml_rich_text')); ?>		
 	</div>
