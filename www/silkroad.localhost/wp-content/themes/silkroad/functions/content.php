6fa3f84374eb26c558edfc9cbb63ac97d565b494
Rectangle <studio@rectangle.design>
2021-03-01 10:56:08 +0000

    fix

--- www/silkroad.localhost/wp-content/themes/silkroad/functions/content.php
+++ www/silkroad.localhost/wp-content/themes/silkroad/functions/content.php
@@ -2,7 +2,7 @@
 
 function silkroad_load_template_part($template_name, $part_name=null, $args=[]) {
 	ob_start();
-	get_template_part($template_name, $part_name, $ar);
+	get_template_part($template_name, $part_name, $args);
 	$var = ob_get_contents();
 	ob_end_clean();
 	return $var;
