aa4ee66ecedd3bba61de9e104bfa6cd9772b2228
Rectangle <studio@rectangle.design>
2021-03-09 06:11:47 +0000

    analytics

--- www/silkroad.localhost/wp-content/themes/silkroad/header.php
+++ www/silkroad.localhost/wp-content/themes/silkroad/header.php
@@ -9,6 +9,15 @@ $classes = get_body_class();
 <html>
 <head>
 	<title>Silk Road</title>
+
+	<!-- Google Tag Manager -->
+	<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+	new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+	j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+	'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+	})(window,document,'script','dataLayer','GTM-M8CGWDS');</script>
+	<!-- End Google Tag Manager -->
+
 	<link rel="stylesheet" type="text/css" href="<?php bloginfo('template_directory');?>/build/styles/screen.css?t=<?php echo time(); ?>" />
 	<script src="<?php bloginfo('template_directory');?>/build/scripts/scripts.js?t=<?php echo time(); ?>"></script>
 	<meta charset="utf-8" />
