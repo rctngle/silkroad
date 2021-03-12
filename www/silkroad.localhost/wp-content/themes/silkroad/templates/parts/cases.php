76f4328d2e795917861f9ac5e88a5758b1157cdc
Lizzie <lizzie@rectangle.design>
2021-03-12 13:43:39 +0000

    update to cases

--- www/silkroad.localhost/wp-content/themes/silkroad/templates/parts/cases.php
+++ www/silkroad.localhost/wp-content/themes/silkroad/templates/parts/cases.php
@@ -10,11 +10,20 @@ $cases_query = new WP_Query([
 	<h1>Cases</h1>
 	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
 </div>
+
 <?php if ($cases_query->have_posts()): ?>
-	<div class="section-inner">		
-		<?php while($cases_query->have_posts()): $cases_query->the_post(); ?>
-			<?php get_template_part('templates/posts/post', get_post_type()); ?>
-		<?php endwhile; ?>
-		<?php wp_reset_postdata() ?>
+	<div class="section-inner-full">	
+		<div class="cases">
+			<?php for($i = 0; $i < 20; $i++):?>			
+				<?php while($cases_query->have_posts()): $cases_query->the_post(); ?>
+					<?php get_template_part('templates/posts/post', get_post_type()); ?>
+				<?php endwhile; ?>
+				<?php wp_reset_postdata() ?>
+			<?php endfor;?>	
+		</div>
+		<div class="gradient">
+			<div class="button" style="background-image: url(https://iran-shutdown.amnesty.org/wp-content/themes/amnesty-iran-internet-shutdown/assets/images/btn-victim-next.png)">
+	        </div>
+        </div>
 	</div>
 <?php endif; ?>
--- www/silkroad.localhost/wp-content/themes/silkroad/templates/posts/post-case.php
+++ www/silkroad.localhost/wp-content/themes/silkroad/templates/posts/post-case.php
@@ -1,70 +1,64 @@
 <article>
 	<div class="preview">
-		<div class="image"></div>
+		<div class="image">
+			<?php the_post_thumbnail();?>
+		</div>
 		<div class="case">
 			
-			<h4><?php echo silkroad_translate_field(get_field('name_ml_text')); ?></h4>
+			<h3><?php echo silkroad_translate_field(get_field('name_ml_text')); ?></h3>
+			<div>
 			<h5>Reference Code:</h5>
-			<h5><?php the_field('reference_code'); ?></h5>
+			<h5 style="color: #000"><?php the_field('reference_code'); ?></h5>
+			</div>
+			<div>
+				<h5>Profession: </h5>
+				<?php echo silkroad_translate_field(get_field('profession_ml_text')); ?>
+			</div>
+
+			<div>
+				<h5>Date of Birth: </h5>
+				<?php the_field('date_of_birth'); ?>
+			</div>
+
+			<div>
+				<h5>Hometown: </h5>
+				<?php echo silkroad_translate_field(get_field('hometown_ml_text')); ?>
+			</div>
+			
+			<div>
+				<h5>Last Seen: </h5>
+				<?php the_field('last_seen_date') ?>
+			</div>
+			<div>
+				<h5>Being Held At:</h5>
+				<?php echo silkroad_translate_field(get_field('where_held_ml_text')); ?>
+			</div>
+			<div class="full">
+				<h5>Real Reason for Arrest or Detention: </h5>
+				<?php echo silkroad_translate_field(get_field('reason_for_arrest_or_detention_suspected_ml_text')); ?>
+			</div>
+			
+			<div class="full">
+				<h5>Official Reason for Arrest or Detention: </h5>
+				<?php echo silkroad_translate_field(get_field('reason_for_arrest_or_detention_official_ml_text')); ?>
+			</div>
+			
+
+			
+			
+			<div class="full">
+				<h5>Quote or Personal Detail:</h5>
+				<?php echo silkroad_translate_field(get_field('quote_or_personal_detail_ml_text')); ?>
+			</div>
+
+			<!-- 
+				<h5>Summary:</h5>
+				<?php echo silkroad_translate_field(get_field('summary_ml_rich_text')); ?> -->
 			
 		</div>
 	</div>
 	<div class="modal">
-		<div>
-			Reference Code: 
-			<?php the_field('reference_code'); ?>
-		</div>
-
-		<div>
-			Name: 
-			<?php echo silkroad_translate_field(get_field('name_ml_text')); ?>
-		</div>
-
-		<div>
-			Profession: 
-			<?php echo silkroad_translate_field(get_field('profession_ml_text')); ?>
-		</div>
-
-		<div>
-			Date of Birth: 
-			<?php the_field('date_of_birth'); ?>
-		</div>
-
-		<div>
-			Hometown: 
-			<?php echo silkroad_translate_field(get_field('hometown_ml_text')); ?>
-		</div>
 		
-		<div>
-			Last Seen: 
-			<?php echo silkroad_translate_field(get_field('last_seen_ml_text')); ?>
-		</div>
-
-		<div>
-			Real Reason for Arrest or Detention: 
-			<?php echo silkroad_translate_field(get_field('reason_for_arrest_or_detention_real_ml_text')); ?>
-		</div>
-		
-		<div>
-			Official Reason for Arrest or Detention: 
-			<?php echo silkroad_translate_field(get_field('reason_for_arrest_or_detention_official_ml_text')); ?>
-		</div>
-		
-
-		<div>
-			Being Held At:
-			<?php echo silkroad_translate_field(get_field('where_held_ml_text')); ?>
-		</div>
-		
-		<div>
-			Quote or Personal Detail:
-			<?php echo silkroad_translate_field(get_field('quote_or_personal_detail_ml_text')); ?>
-		</div>
-
-		<div>
-			Summary:
-			<?php echo silkroad_translate_field(get_field('summary_ml_rich_text')); ?>
-		</div>
 
 
 		<?php
