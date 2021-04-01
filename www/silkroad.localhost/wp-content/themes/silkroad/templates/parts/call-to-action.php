9c730b3166b37a5f844d1e9d21c441b5f18eeaff
Lizzie <lizzie@rectangle.design>
2021-04-01 10:51:40 +0100

    updates for call to action

--- www/silkroad.localhost/wp-content/themes/silkroad/templates/parts/call-to-action.php
+++ www/silkroad.localhost/wp-content/themes/silkroad/templates/parts/call-to-action.php
@@ -6,7 +6,17 @@
 
 	<div class="content-box">
 			<div class="section-title centered">
-			<h1>Take Action</h1>
+			<h1 class="sub-title">Write to the leaders of the Coalition and demand accountability</h1>
+
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+			consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+			cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+			proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+			<div class="take-action-button">
+				Take Action
+			</div>
 		</div>
 		<iframe src="https://join.amnesty.org/page/40993/action/1?Locale=en-US"></iframe>
 	</div>
