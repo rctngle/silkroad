0bfea7561ffd3e42f3bfd343b4be5352a2dd3264
rctngle <studio@rectangle.design>
2021-05-06 12:45:58 +0100

    htumbs

--- www/silkroad.localhost/wp-content/themes/silkroad/templates/parts/case-pager.php
+++ www/silkroad.localhost/wp-content/themes/silkroad/templates/parts/case-pager.php
@@ -1,5 +1,17 @@
+<?php 
+
+$thumb_url = get_the_post_thumbnail_url(get_the_ID(), 'thumbnail');
+$image_cropped = get_field('image_cropped');
+if ($image_cropped) {
+	$thumb_url = $image_cropped['sizes']['thumbnail'];
+}
+
+?>
 <div class="case-pagination-item">
+
 	<div class="image">
-		<img src="<?php echo get_field('image_cropped')['sizes']['thumbnail'];?>"/>
+		<a href="#case-<?php the_field('reference_code'); ?>">
+			<img src="<?php echo $thumb_url; ?>"/>
+		</a>
 	</div>
 </div>
