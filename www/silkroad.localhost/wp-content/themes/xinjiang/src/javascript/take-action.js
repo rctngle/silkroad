e56eaec36af7f7190e8fdc48dab8fb2b5a58f5c1
rctngle <studio@rectangle.design>
2022-04-08 13:25:41 +0100

    updates

--- www/silkroad.localhost/wp-content/themes/xinjiang/src/javascript/take-action.js
+++ www/silkroad.localhost/wp-content/themes/xinjiang/src/javascript/take-action.js
@@ -2,7 +2,7 @@ export default function createTakeAction(){
 	document.querySelectorAll('.take-action-container').forEach(container => {
 		container.querySelectorAll('.take-action-button, .take-action').forEach(actionButton=>{
 			actionButton.addEventListener('click', e=>{
-				container.classList.add('display-iframe');
+				// container.classList.add('display-iframe');
 			});
 		});
 	});
